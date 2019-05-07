70198c237e2c8225d4131340832a38eeb97eb2a0
jj97181818 <jj97181818@gmail.com>
2019-05-07 15:53:45 +0800

    Add Bottom Menu on Main Activity

--- app/src/main/java/me/jj97181818/carbonpoint/MainActivity.java
+++ app/src/main/java/me/jj97181818/carbonpoint/MainActivity.java
@@ -1,20 +1,52 @@
 package me.jj97181818.carbonpoint;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+    private View v;
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    return true;
+                case R.id.navigation_dashboard:
+                    gotoMemberActivity(v);
+                    return true;
+                case R.id.navigation_notifications:
+                    gotoNoticeActivity(v);
+                    return true;
+            }
+            return false;
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 
-    public void gotoSpecificActivity(View v) {
-        Intent it = new Intent(this, test.class);
+    public void gotoMemberActivity(View v) {
+        Intent it = new Intent(this, MemberDataActivity.class);
+        startActivity(it);
+    }
+    public void gotoNoticeActivity(View v) {
+        Intent it = new Intent(this, NoticeActivity.class);
         startActivity(it);
     }
 }

0dd59bac77d08f8e56d829e750a4cf1783817af5
jj97181818 <jj97181818@gmail.com>
2019-05-08 10:11:27 +0800

    Add RecyclerView on GiftAcitivity

--- app/src/main/java/me/jj97181818/carbonpoint/GiftActivity.java
+++ app/src/main/java/me/jj97181818/carbonpoint/GiftActivity.java
@@ -5,16 +5,25 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Pair;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.ListAdapter;
-import android.widget.ListView;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class GiftActivity extends AppCompatActivity {
-    private ListView listView;
-    private ListAdapter listAdapter;
     private View v;
+    private GiftActivity.MyAdapter myAdapter;
+    private RecyclerView myRecyclerView;
+
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
@@ -44,10 +53,71 @@ public class GiftActivity extends AppCompatActivity {
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
+        //list
+        ArrayList<Pair<Integer, String>> myDataset = new ArrayList<>();
 
-        listView = (ListView) findViewById(R.id.listView);
-        listAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, new String[]{"一", "二", "三"});
-        listView.setAdapter(listAdapter);
+        myDataset.add(new Pair<>(R.drawable.salad, "任一沙拉\n" + "30 點特價 210 元"));
+        myDataset.add(new Pair<>(R.drawable.hamburger, "中東香料漢堡 \n" + "20 點特價 290 元 "));
+        myDataset.add(new Pair<>(R.drawable.pasta, "野菇蒙太奇義大利麵\n" + "25 點特價 320 元 "));
+        myDataset.add(new Pair<>(R.drawable.sandwich, "元氣雙層蔬菜三明治 \n" + "20 點特價 290 元 "));
+        myDataset.add(new Pair<>(R.drawable.drink, "任一手打飲品 \n" + "20 點享八折優惠 "));
+
+        myAdapter = new GiftActivity.MyAdapter(myDataset);
+        myRecyclerView = (RecyclerView) findViewById(R.id.list_view2);
+        final LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        myRecyclerView.setLayoutManager(layoutManager);
+        myRecyclerView.setAdapter(myAdapter);
+    }
+
+    public class MyAdapter extends RecyclerView.Adapter<GiftActivity.MyAdapter.ViewHolder> {
+        private List<Pair<Integer, String>> mData;
+
+        public class ViewHolder extends RecyclerView.ViewHolder {
+            public TextView mTextView;
+            public ImageView mImageView;
+            public ViewHolder(View v) {
+                super(v);
+                mTextView = (TextView) v.findViewById(R.id.food_text);
+                mImageView = (ImageView) v.findViewById(R.id.food_img);
+            }
+        }
+
+        public MyAdapter(List<Pair<Integer, String>> data) {
+            mData = data;
+        }
+
+        @Override
+        public GiftActivity.MyAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View v = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.giftlayout, parent, false);
+            GiftActivity.MyAdapter.ViewHolder vh = new GiftActivity.MyAdapter.ViewHolder(v);
+            return vh;
+        }
+
+        @Override
+        public void onBindViewHolder(GiftActivity.MyAdapter.ViewHolder holder, final int position) {
+            holder.mImageView.setImageDrawable(getDrawable(mData.get(position).first));
+            holder.mTextView.setText(mData.get(position).second);
+            holder.itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(GiftActivity.this, "第 " + position + " 個被選擇了！", Toast.LENGTH_SHORT).show();
+                }
+            });
+            holder.itemView.setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View v) {
+                    Toast.makeText(GiftActivity.this, "第 " + position + " 個被長按了！", Toast.LENGTH_SHORT).show();
+                    return true;
+                }
+            });
+        }
+
+        @Override
+        public int getItemCount() {
+            return mData.size();
+        }
     }
 
     public void gotoMainActivity(View v) {

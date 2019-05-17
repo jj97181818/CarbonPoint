bcfd415cbf4b546b72be0d0f6b35c6e4caaca4ea
Jessie Chen <jessyechen@gmail.com>
2019-05-17 17:24:16 +0800

    add login activity / layout add register activity / layout

--- app/src/main/java/me/jj97181818/carbonpoint/RegisterActivity.java
+++ app/src/main/java/me/jj97181818/carbonpoint/RegisterActivity.java
@@ -1,13 +1,133 @@
 package me.jj97181818.carbonpoint;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
+import android.support.design.widget.TextInputLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
 
 public class RegisterActivity extends AppCompatActivity {
 
+    EditText editTextUserName;
+    EditText editTextEmail;
+    EditText editTextPassword;
+
+    TextInputLayout textInputLayoutUserName;
+    TextInputLayout textInputLayoutEmail;
+    TextInputLayout textInputLayoutPassword;
+
+    Button buttonRegister;
+
+    SqliteHelper sqliteHelper;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
+        setContentView(me.jj97181818.carbonpoint.R.layout.activity_register);
+        sqliteHelper = new SqliteHelper(this);
+        initTextViewLogin();
+        initViews();
+        buttonRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (validate()) {
+                    String UserName = editTextUserName.getText().toString();
+                    String Email = editTextEmail.getText().toString();
+                    String Password = editTextPassword.getText().toString();
+
+                    if (!sqliteHelper.isEmailExists(Email)) {
+
+                        sqliteHelper.addUser(new User(null, UserName, Email, Password));
+                        Snackbar.make(buttonRegister, "User created successfully! Please Login ", Snackbar.LENGTH_LONG).show();
+                        new Handler().postDelayed(new Runnable() {
+                            @Override
+                            public void run() {
+                                finish();
+                            }
+                        }, Snackbar.LENGTH_LONG);
+                    }else {
+
+                        Snackbar.make(buttonRegister, "User already exists with same email ", Snackbar.LENGTH_LONG).show();
+                    }
+
+
+                }
+            }
+        });
+    }
+
+    private void initTextViewLogin() {
+        TextView textViewLogin = (TextView) findViewById(me.jj97181818.carbonpoint.R.id.textViewLogin);
+        textViewLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
+
+    private void initViews() {
+        editTextEmail = (EditText) findViewById(me.jj97181818.carbonpoint.R.id.editTextEmail);
+        editTextPassword = (EditText) findViewById(me.jj97181818.carbonpoint.R.id.editTextPassword);
+        editTextUserName = (EditText) findViewById(me.jj97181818.carbonpoint.R.id.editTextUserName);
+        textInputLayoutEmail = (TextInputLayout) findViewById(me.jj97181818.carbonpoint.R.id.textInputLayoutEmail);
+        textInputLayoutPassword = (TextInputLayout) findViewById(me.jj97181818.carbonpoint.R.id.textInputLayoutPassword);
+        textInputLayoutUserName = (TextInputLayout) findViewById(me.jj97181818.carbonpoint.R.id.textInputLayoutUserName);
+        buttonRegister = (Button) findViewById(me.jj97181818.carbonpoint.R.id.buttonRegister);
+
+    }
+
+
+    public boolean validate() {
+        boolean valid = false;
+
+
+        String UserName = editTextUserName.getText().toString();
+        String Email = editTextEmail.getText().toString();
+        String Password = editTextPassword.getText().toString();
+
+        if (UserName.isEmpty()) {
+            valid = false;
+            textInputLayoutUserName.setError("Please enter valid username!");
+        } else {
+            if (UserName.length() > 5) {
+                valid = true;
+                textInputLayoutUserName.setError(null);
+            } else {
+                valid = false;
+                textInputLayoutUserName.setError("Username is to short!");
+            }
+        }
+
+
+        if (!android.util.Patterns.EMAIL_ADDRESS.matcher(Email).matches()) {
+            valid = false;
+            textInputLayoutEmail.setError("Please enter valid email!");
+        } else {
+            valid = true;
+            textInputLayoutEmail.setError(null);
+        }
+
+        if (Password.isEmpty()) {
+            valid = false;
+            textInputLayoutPassword.setError("Please enter valid password!");
+        } else {
+            if (Password.length() > 5) {
+                valid = true;
+                textInputLayoutPassword.setError(null);
+            } else {
+                valid = false;
+                textInputLayoutPassword.setError("Password is to short!");
+            }
+        }
+
+
+        return valid;
     }
 }
---
+++ app/src/main/java/me/jj97181818/carbonpoint/SqliteHelper.java
@@ -0,0 +1,100 @@
+package me.jj97181818.carbonpoint;
+
+
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import me.jj97181818.carbonpoint.User;
+
+
+public class SqliteHelper extends SQLiteOpenHelper {
+
+
+    public static final String DATABASE_NAME = "Noter";
+
+    public static final int DATABASE_VERSION = 1;
+
+    public static final String TABLE_USERS = "users";
+
+    public static final String KEY_ID = "id";
+
+    public static final String KEY_USER_NAME = "username";
+
+    public static final String KEY_EMAIL = "email";
+
+    public static final String KEY_PASSWORD = "password";
+
+
+    public static final String SQL_TABLE_USERS = " CREATE TABLE " + TABLE_USERS  + " ( "
+            + KEY_ID + " INTEGER PRIMARY KEY, "
+            + KEY_USER_NAME + " TEXT, "
+            + KEY_EMAIL + " TEXT, "
+            + KEY_PASSWORD + " TEXT"
+            + " ) ";
+
+
+    public SqliteHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        sqLiteDatabase.execSQL(SQL_TABLE_USERS);
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        sqLiteDatabase.execSQL(" DROP TABLE IF EXISTS " + TABLE_USERS);
+    }
+
+    public void addUser(User user) {
+
+
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(KEY_USER_NAME, user.userName);
+        values.put(KEY_EMAIL, user.email);
+        values.put(KEY_PASSWORD, user.password);
+        long todo_id = db.insert(TABLE_USERS, null, values);
+    }
+
+    public User Authenticate(User user) {
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.query(TABLE_USERS,
+                new String[]{KEY_ID, KEY_USER_NAME, KEY_EMAIL, KEY_PASSWORD},
+                KEY_EMAIL + "=?",
+                new String[]{user.email},
+                null, null, null);
+
+        if (cursor != null && cursor.moveToFirst()) {
+            User user1 = new User(cursor.getString(0), cursor.getString(1), cursor.getString(2), cursor.getString(3));
+
+            if (user.password.equalsIgnoreCase(user1.password)) {
+                return user1;
+            }
+        }
+
+        return null;
+    }
+
+    public boolean isEmailExists(String email) {
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.query(TABLE_USERS,
+                new String[]{KEY_ID, KEY_USER_NAME, KEY_EMAIL, KEY_PASSWORD},
+                KEY_EMAIL + "=?",
+                new String[]{email},
+                null, null, null);
+
+        if (cursor != null && cursor.moveToFirst()) {
+            return true;
+        }
+
+        return false;
+    }
+}
+
